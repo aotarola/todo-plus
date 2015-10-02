60569d068b7ce5eb4d51be7ca809dc5be4a034a1
Andres Otarola Alvarado <aotarola@groupon.com>
2015-10-01 19:51:51 -0700

    Added date picker

--- app/src/main/java/com/example/aotarolaalvarad/todoapp/EditItemDialog.java
+++ app/src/main/java/com/example/aotarolaalvarad/todoapp/EditItemDialog.java
@@ -1,17 +1,25 @@
 package com.example.aotarolaalvarad.todoapp;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnFocusChangeListener;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.support.v4.app.DialogFragment;
 import android.widget.Spinner;
 import android.view.View.OnClickListener;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.TimeZone;
 
 
 /**
@@ -27,6 +35,17 @@ public class EditItemDialog extends DialogFragment implements OnClickListener {
     private Spinner spPriority;
 
     ArrayAdapter<TodoItem.Priority> priorityAdapter;
+
+    private DatePickerDialog.OnDateSetListener datePickerListener = new DatePickerDialog.OnDateSetListener() {
+
+        public void onDateSet(DatePicker view, int selectedYear,
+                              int selectedMonth, int selectedDay) {
+            String year1 = String.valueOf(selectedYear);
+            String month1 = String.valueOf(selectedMonth + 1);
+            String day1 = String.valueOf(selectedDay);
+            etDueDate.setText(day1 + "/" + month1 + "/" + year1);
+        }
+    };
 
     public interface EditItemDialogListener {
         void onFinishEditDialog(String title, String dueDate, TodoItem.Priority priority, int position);
@@ -63,13 +82,23 @@ public class EditItemDialog extends DialogFragment implements OnClickListener {
         btnCancel = (Button) view.findViewById(R.id.btnCancel);
         btnSave = (Button) view.findViewById(R.id.btnUpdateItem);
 
-        btnCancel.setOnClickListener(this);
-        btnSave.setOnClickListener(this);
 
         etTitle = (EditText) view.findViewById(R.id.etEditItemText);
         etDueDate = (EditText) view.findViewById(R.id.etEditDueDateText);
         spPriority = (Spinner) view.findViewById(R.id.spinner);
 
+
+        //Set listeners
+
+        btnCancel.setOnClickListener(this);
+        btnSave.setOnClickListener(this);
+        etDueDate.setOnClickListener(this);
+        etDueDate.setOnFocusChangeListener(new OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                showCalendarDialog();
+            }
+        });
 
         priorityAdapter = new ArrayAdapter<TodoItem.Priority>(getActivity(),
                 android.R.layout.simple_list_item_1, TodoItem.Priority.values());
@@ -85,6 +114,19 @@ public class EditItemDialog extends DialogFragment implements OnClickListener {
         etTitle.requestFocus();
         getDialog().getWindow().setSoftInputMode(
                 WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+    }
+
+    private void showCalendarDialog(){
+        Calendar cal = Calendar.getInstance(TimeZone.getDefault()); // Get current date
+
+        DatePickerDialog datePicker = new DatePickerDialog(getActivity(),
+                R.style.AppTheme, datePickerListener,
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH));
+        datePicker.setCancelable(false);
+        datePicker.setTitle(R.string.select_the_date);
+        datePicker.show();
     }
 
     @Override
@@ -104,6 +146,9 @@ public class EditItemDialog extends DialogFragment implements OnClickListener {
                 );
 
                 this.dismiss();
+                break;
+            case R.id.etEditDueDateText:
+                showCalendarDialog();
                 break;
             default:
                 break;
